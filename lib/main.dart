d368c35b08969bfdacf5d566c23b20b4c41b7f7d
Song Yang <songyang.dev@gmail.com>
2023-09-19 14:18:11 -0400

    Anonymous sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const FirebaseDemonstration());
 }
 
-class FirebaseDemonstration extends StatelessWidget {
+class FirebaseDemonstration extends StatefulWidget {
   const FirebaseDemonstration({super.key});
 
   @override
+  State<FirebaseDemonstration> createState() => _FirebaseDemonstrationState();
+}
+
+class _FirebaseDemonstrationState extends State<FirebaseDemonstration> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Future<FirebaseApp> initialization = Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    return MaterialApp(
+      home: FutureBuilder(
+        future: initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const Text("Connected");
+          }
+          return const Text("Loading");
+        },
+      ),
+    );
   }
 }
