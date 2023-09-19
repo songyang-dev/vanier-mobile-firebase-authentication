51858f696d6d6fb33667621e1e9570ae26cb3414
Song Yang <songyang.dev@gmail.com>
2023-09-18 22:27:08 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FirebaseDemonstration());
+}
+
+class FirebaseDemonstration extends StatelessWidget {
+  const FirebaseDemonstration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
